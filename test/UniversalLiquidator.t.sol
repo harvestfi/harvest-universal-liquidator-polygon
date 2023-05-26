7630dd01c5671b6e3035472609a44de92913f6e9
ryanycw <ryan@ryanycw.dev>
2023-05-26 20:18:39 +0800

    test: finish single swap test

--- test/UniversalLiquidator.t.sol
+++ test/UniversalLiquidator.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
 
-import "forge-std/console2.sol";
+import "forge-std/Test.sol";
 
 // interfaces
 import "openzeppelin/token/ERC20/IERC20.sol";
@@ -10,23 +10,22 @@ import "openzeppelin/token/ERC20/IERC20.sol";
 import {AdvancedFixture} from "./AdvancedFixture.t.sol";
 
 contract UniversalLiquidatorTest is AdvancedFixture {
-    uint256 _polygonFork;
     address _farmer;
 
     function setUp() public {
-        // fork testing environment
-        _polygonFork = vm.createFork(_POLYGON_RPC_URL);
-        vm.selectFork(_polygonFork);
         // mock farmer address
         _farmer = makeAddr("farmer");
     }
 
     function testSwapWithSingleStep() public {
         for (uint256 i; i < _singleTokenPairCount;) {
+            uint256 snapshot = vm.snapshot();
             // whale transfer token to farmer
-            uint256 sellAmount = IERC20(_singleTokenPairs[i].sellToken).balanceOf(_singleTokenPairs[0].whale);
-            vm.prank(_singleTokenPairs[0].whale);
-            IERC20(address(_singleTokenPairs[0].sellToken)).transfer(_farmer, sellAmount);
+            uint256 sellAmount = IERC20(_singleTokenPairs[i].sellToken).balanceOf(_singleTokenPairs[i].whale) / 100;
+            console.log("Sell amount: %d", sellAmount);
+            uint256 minBuyAmount = 1; // * 10 ** 18;
+            vm.prank(_singleTokenPairs[i].whale);
+            IERC20(address(_singleTokenPairs[i].sellToken)).transfer(_farmer, sellAmount);
 
             // check farmer, deployedDex, and ul token balance
             assertEq(sellAmount, IERC20(_singleTokenPairs[i].sellToken).balanceOf(_farmer));
@@ -44,11 +43,35 @@ contract UniversalLiquidatorTest is AdvancedFixture {
                 }
             }
             // approve ul as farmer
+            startHoax(_farmer);
+            IERC20(_singleTokenPairs[i].sellToken).approve(address(_universalLiquidator), sellAmount);
             // swap
+            uint256 receiveAmt = _universalLiquidator.swap(
+                _singleTokenPairs[i].sellToken, _singleTokenPairs[i].buyToken, sellAmount, minBuyAmount, _farmer
+            );
+            console.log(receiveAmt);
             // check farmer, deployedDex, and ul token balance
+            assertEq(0, IERC20(_singleTokenPairs[i].sellToken).balanceOf(_farmer));
+            assertEq(0, IERC20(_singleTokenPairs[i].sellToken).balanceOf(address(_universalLiquidator)));
+
+            uint256 postSwapBalance = IERC20(_singleTokenPairs[i].buyToken).balanceOf(_farmer);
+            assertLt(minBuyAmount, receiveAmt);
+            assertLt(minBuyAmount, postSwapBalance);
+            assertLe(postSwapBalance, receiveAmt);
+            assertEq(0, IERC20(_singleTokenPairs[i].buyToken).balanceOf(address(_universalLiquidator)));
+
+            for (uint256 j; j < requiredDex;) {
+                address deployedDex = _dexesByName[_singleTokenPairs[i].dexSetup[j].dexName].addr;
+                assertEq(0, IERC20(_singleTokenPairs[i].sellToken).balanceOf(deployedDex));
+                assertEq(0, IERC20(_singleTokenPairs[i].buyToken).balanceOf(deployedDex));
+                unchecked {
+                    ++j;
+                }
+            }
             unchecked {
                 ++i;
             }
+            vm.revertTo(snapshot);
         }
     }
 
