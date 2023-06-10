c45056648184d88026f9193aef73b8a656cafd41
ryanycw <ryan@ryanycw.dev>
2023-06-10 19:54:31 +0800

    chore: setup the system parameters

---
+++ script/config/Fees.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+struct FeePair {
+    address sellToken;
+    address buyToken;
+    string dexName;
+    uint256 fee;
+}
+
+abstract contract Fees {
+    uint256 internal _feePairsCount;
+    mapping(uint256 => FeePair) internal _fees;
+
+    constructor() {
+        // Pool0 - DAI -> USDC
+        FeePair storage newFee = _fees[_feePairsCount++];
+        newFee.sellToken = 0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619;
+        newFee.buyToken = 0xB0B195aEFA3650A6908f15CdaC7D92F8a5791B0B;
+        newFee.dexName = "UniV3Dex";
+        newFee.fee = 500;
+
+        // Pool1 - WMATIC -> USDC
+        newFee = _fees[_feePairsCount++];
+        newFee.sellToken = 0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270;
+        newFee.buyToken = 0xB0B195aEFA3650A6908f15CdaC7D92F8a5791B0B;
+        newFee.dexName = "UniV3Dex";
+        newFee.fee = 500;
+
+        // Pool2 - WMATIC -> WETH
+        newFee = _fees[_feePairsCount++];
+        newFee.sellToken = 0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270;
+        newFee.buyToken = 0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619;
+        newFee.dexName = "UniV3Dex";
+        newFee.fee = 500;
+    }
+}
