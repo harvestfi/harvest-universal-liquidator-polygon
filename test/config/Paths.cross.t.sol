8447eb9fefd9fd36e0f618115a341f813188d4bc
ryanycw <ryan@ryanycw.dev>
2023-06-12 10:58:11 +0800

    refactor: adjust testing parameters to improve consistency

--- test/config/Paths.cross.t.sol
+++ test/config/Paths.cross.t.sol
@@ -22,8 +22,8 @@ abstract contract CrossDexSwapPaths {
         newTokenPair.buyToken = 0x9a71012B13CA4d3D0Cdc72A177DF3ef03b0E76A3;
         newTokenPair.intermediateToken = address(0);
         newTokenPair.whale = 0x21Cb017B40abE17B6DFb9Ba64A3Ab0f24A7e60EA;
-        newTokenPair.dexSetup.push(Types.DexSetting("uniV3", _pathA));
-        newTokenPair.dexSetup.push(Types.DexSetting("balancer", _pathB));
+        newTokenPair.dexSetup.push(Types.DexSetting("UniV3Dex", _pathA));
+        newTokenPair.dexSetup.push(Types.DexSetting("BalancerDex", _pathB));
 
         // Pair1 - (UniV3) WMATIC -> WETH -> AAVE (Sushi)
         _pathA[0] = 0x385Eeac5cB85A38A9a07A70c73e0a3271CfB54A7;
@@ -36,8 +36,8 @@ abstract contract CrossDexSwapPaths {
         newTokenPair.buyToken = 0xD6DF932A45C0f255f85145f286eA0b292B21C90B;
         newTokenPair.intermediateToken = address(0);
         newTokenPair.whale = 0x21Cb017B40abE17B6DFb9Ba64A3Ab0f24A7e60EA;
-        newTokenPair.dexSetup.push(Types.DexSetting("uniV3", _pathA));
-        newTokenPair.dexSetup.push(Types.DexSetting("sushi", _pathB));
+        newTokenPair.dexSetup.push(Types.DexSetting("UniV3Dex", _pathA));
+        newTokenPair.dexSetup.push(Types.DexSetting("SushiswapDex", _pathB));
 
         // Pair2 - (UniV3) UNI -> USDC -> USDT (Curve)
         _pathA[0] = 0xb33EaAd8d922B1083446DC23f610c2567fB5180f;
@@ -50,7 +50,7 @@ abstract contract CrossDexSwapPaths {
         newTokenPair.buyToken = 0xc2132D05D31c914a87C6611C10748AEb04B58e8F;
         newTokenPair.intermediateToken = address(0);
         newTokenPair.whale = 0x06959153B974D0D5fDfd87D561db6d8d4FA0bb0B;
-        newTokenPair.dexSetup.push(Types.DexSetting("uniV3", _pathA));
-        newTokenPair.dexSetup.push(Types.DexSetting("curve", _pathB));
+        newTokenPair.dexSetup.push(Types.DexSetting("UniV3Dex", _pathA));
+        newTokenPair.dexSetup.push(Types.DexSetting("CurveDex", _pathB));
     }
 }
--- test/config/Paths.single.t.sol
+++ test/config/Paths.single.t.sol
@@ -19,7 +19,7 @@ abstract contract SingleSwapPaths {
         newTokenPair.buyToken = 0xB0B195aEFA3650A6908f15CdaC7D92F8a5791B0B;
         newTokenPair.intermediateToken = address(0);
         newTokenPair.whale = 0x62ac55b745F9B08F1a81DCbbE630277095Cf4Be1;
-        newTokenPair.dexSetup.push(Types.DexSetting("uniV3", _path));
+        newTokenPair.dexSetup.push(Types.DexSetting("UniV3Dex", _path));
 
         // Pair1 - WMATIC -> stMATIC
         _path[0] = 0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270;
@@ -30,7 +30,7 @@ abstract contract SingleSwapPaths {
         newTokenPair.buyToken = 0x3A58a54C066FdC0f2D55FC9C89F0415C92eBf3C4;
         newTokenPair.intermediateToken = address(0);
         newTokenPair.whale = 0x6e7a5FAFcec6BB1e78bAE2A1F0B612012BF14827;
-        newTokenPair.dexSetup.push(Types.DexSetting("balancer", _path));
+        newTokenPair.dexSetup.push(Types.DexSetting("BalancerDex", _path));
 
         // Pair2 - WMATIC -> SUSHI
         _path[0] = 0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270;
@@ -41,7 +41,7 @@ abstract contract SingleSwapPaths {
         newTokenPair.buyToken = 0x0b3F868E0BE5597D5DB7fEB59E1CADBb0fdDa50a;
         newTokenPair.intermediateToken = address(0);
         newTokenPair.whale = 0x6e7a5FAFcec6BB1e78bAE2A1F0B612012BF14827;
-        newTokenPair.dexSetup.push(Types.DexSetting("sushi", _path));
+        newTokenPair.dexSetup.push(Types.DexSetting("SushiswapDex", _path));
 
         // Pair3 - USDC -> DAI
         _path[0] = 0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174;
@@ -52,6 +52,6 @@ abstract contract SingleSwapPaths {
         newTokenPair.buyToken = 0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063;
         newTokenPair.intermediateToken = address(0);
         newTokenPair.whale = 0x72A53cDBBcc1b9efa39c834A540550e23463AAcB;
-        newTokenPair.dexSetup.push(Types.DexSetting("curve", _path));
+        newTokenPair.dexSetup.push(Types.DexSetting("CurveDex", _path));
     }
 }
