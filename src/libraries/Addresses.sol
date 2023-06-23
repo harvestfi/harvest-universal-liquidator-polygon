9a47075e7bb7c5abf20b1aca152a2ea340e30742
CryptJS13 <jasper@slimmens.nl>
2023-06-23 12:44:11 +0200

    Add Quickswap and deploy

--- src/libraries/Addresses.sol
+++ src/libraries/Addresses.sol
@@ -5,5 +5,6 @@ library Addresses {
     address constant uniswapV3Router = 0xE592427A0AEce92De3Edee1F18E0157C05861564;
     address constant balancerVault = 0xBA12222222228d8Ba445958a75a0704d566BF2C8;
     address constant sushiSwapRouter = 0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506;
+    address constant quickSwapRouter = 0xa5E0829CaCEd8fFDD4De3c43696c57F7D7A678ff;
     address constant curveRouter = 0x2a426b3Bb4fa87488387545f15D01d81352732F9;
 }
